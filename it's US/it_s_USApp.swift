85bd35428017ea5e505be0636098d3ec021b2743
Egor Zheliba <e.zheliba@vkteam.ru>
2025-05-02 18:04:15 +0300

    Initial Commit

---
+++ it's US/it_s_USApp.swift
@@ -0,0 +1,17 @@
+//
+//  it_s_USApp.swift
+//  it's US
+//
+//  Created by Egor Zheliba on 02.05.2025.
+//
+
+import SwiftUI
+
+@main
+struct it_s_USApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
